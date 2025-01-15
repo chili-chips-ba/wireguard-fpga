cc929a56ba803d83b8735d3f446b5ad35945f06f
Enio Kaljic <enio.kaljic@gmail.com>
2025-01-15 14:03:28 +0100

    Added DPE WireGuard Disassembler

---
+++ 1.hw/ip.dpe/dpe_wg_disassembler.sv
@@ -0,0 +1,285 @@
+//==========================================================================
+// Copyright (C) 2024-2025 Chili.CHIPS*ba
+//--------------------------------------------------------------------------
+//                      PROPRIETARY INFORMATION
+//
+// The information contained in this file is the property of CHILI CHIPS LLC.
+// Except as specifically authorized in writing by CHILI CHIPS LLC, the holder
+// of this file: (1) shall keep all information contained herein confidential;
+// and (2) shall protect the same in whole or in part from disclosure and
+// dissemination to all third parties; and (3) shall use the same for operation
+// and maintenance purposes only.
+//--------------------------------------------------------------------------
+// Description: 
+//   DPE WireGuard Diassembler
+//==========================================================================
+
+module dpe_wg_disassembler (
+    input  logic      clk,
+    input  logic      rst,
+    
+    output logic      is_idle,
+    
+    dpe_if.s_axis     inp,
+    dpe_if.m_axis     outp
+);
+    localparam TDATA_WIDTH = 128;
+    localparam INP_TUSER_WIDTH = 5;
+    localparam OUTP_TUSER_WIDTH = 128;
+	
+	localparam PROT_IP = 16'h0008;
+	localparam PROT_IPv4 = 4'h4;
+	localparam PROT_UDP = 8'h11;
+	localparam PROT_WGD = 8'h04;
+	
+	typedef enum logic [3:0] {
+        IDLE,
+        HEADER_1,
+		HEADER_2,
+		BYPASS_1,
+		BYPASS_2,
+		BYPASS_3,
+		PAYLOAD_1,
+		PAYLOAD_2,
+		PAYLOAD_3,
+		PAYLOAD_4,
+		PAYLOAD_5
+    } state_t;
+    
+    // Internal states
+	state_t state, next_state;
+	logic [31:0] wg_rcv, next_wg_rcv;
+	logic [63:0] wg_cnt, next_wg_cnt;
+	logic [47:0] wg_enp, next_wg_enp;
+    
+	// Internal wires
+    dpe_if #(TDATA_WIDTH, INP_TUSER_WIDTH) s_inp();
+	dpe_if #(TDATA_WIDTH, OUTP_TUSER_WIDTH) s_outp();
+    
+	// FSM registers
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            state <= IDLE;
+			wg_rcv <= '0;
+			wg_cnt <= '0;
+			wg_enp <= '0;
+        end else begin
+            state <= next_state;
+			wg_rcv <= next_wg_rcv;
+			wg_cnt <= next_wg_cnt;
+			wg_enp <= next_wg_enp;
+        end
+    end
+	
+	// FSM transition logic
+    always_comb begin
+        next_state = state;
+        next_wg_rcv = wg_rcv;
+		next_wg_cnt = wg_cnt;
+		next_wg_enp = wg_enp;
+        
+		case (state)
+            IDLE: begin
+                if (inp.tvalid && inp.tready && (inp.tdata[111:96] == PROT_IP) && (inp.tdata[119:116] == PROT_IPv4)) begin
+					next_state = HEADER_1;
+				end else if (inp.tvalid && inp.tready) begin
+					next_state = BYPASS_1;
+				end
+            end
+            
+            HEADER_1: begin
+                if (inp.tvalid && inp.tready && (inp.tdata[63:56] == PROT_UDP)) begin
+					next_state = HEADER_2;
+				end else if (inp.tvalid && inp.tready) begin
+					next_state = BYPASS_2;
+				end
+            end
+			
+			HEADER_2: begin
+                if (inp.tvalid && inp.tready && (inp.tdata[87:80] == PROT_WGD)) begin
+					next_state = PAYLOAD_1;
+					next_wg_rcv[15:0] = inp.tdata[127:112];
+				end else if (inp.tvalid && inp.tready) begin
+					next_state = BYPASS_3;
+					next_wg_rcv = '0;
+				end
+            end
+			
+			BYPASS_1: begin
+                if (inp.tvalid && inp.tready) begin
+					next_state = BYPASS_2;
+				end
+            end
+			
+			BYPASS_2: begin
+                if (inp.tvalid && inp.tready) begin
+					next_state = BYPASS_3;
+				end
+            end
+			
+			BYPASS_3: begin
+                if (inp.tvalid && inp.tready && inp.tlast) begin
+					next_state = IDLE;
+				end
+            end
+			
+			PAYLOAD_1: begin
+                if (inp.tvalid && inp.tready) begin
+					next_state = PAYLOAD_2;
+					next_wg_rcv[31:16] = inp.tdata[15:0];
+					next_wg_cnt = inp.tdata[79:16];
+				end
+            end
+			
+			PAYLOAD_2: begin
+                if (inp.tvalid && inp.tready) begin
+					next_state = PAYLOAD_3;
+				end
+            end
+			
+			PAYLOAD_3: begin
+                if (s_inp.tvalid && s_inp.tready) begin
+					next_state = PAYLOAD_4;
+				end
+            end
+			
+			PAYLOAD_4: begin
+                if (s_inp.tvalid && s_inp.tready && s_inp.tlast) begin
+					next_state = IDLE;
+					next_wg_enp = s_inp.tdata[127:80];
+				end else if (s_inp.tvalid && s_inp.tready) begin
+					next_state = PAYLOAD_5;
+					next_wg_enp = s_inp.tdata[127:80];
+				end
+            end
+			
+			PAYLOAD_5: begin
+                if (s_inp.tvalid && s_inp.tready && s_inp.tlast) begin
+					next_state = IDLE;
+					next_wg_enp = s_inp.tdata[127:80];
+				end else if (s_inp.tvalid && s_inp.tready) begin
+					next_wg_enp = s_inp.tdata[127:80];
+				end
+            end
+			
+            default: begin
+                next_state = state;
+				next_wg_rcv = wg_rcv;
+				next_wg_cnt = wg_cnt;
+				next_wg_enp = wg_enp;
+			end
+        endcase
+    end
+	
+	// Outputs logic
+    always_comb begin
+		s_outp.tvalid = 0;
+		s_outp.tdata = '0;
+		s_outp.tkeep = '0;
+		s_outp.tlast = 0;
+		s_outp.tuser = '0;
+        case (state)
+			IDLE, HEADER_1, HEADER_2: begin
+				if (wg_rcv == '0) begin
+					s_outp.tvalid = s_inp.tvalid;
+					s_outp.tdata = s_inp.tdata;
+					s_outp.tkeep = s_inp.tkeep;
+					s_outp.tlast = s_inp.tlast;
+					s_outp.tuser[4:0] = s_inp.tuser;
+				end else begin
+					s_outp.tvalid = s_inp.tvalid;
+					s_outp.tdata[127:48] = s_inp.tdata[79:0];
+					s_outp.tdata[47:0] = wg_enp;
+					s_outp.tkeep = '1;
+					s_outp.tlast = s_inp.tlast;
+					s_outp.tuser[127:96] = wg_rcv;
+					s_outp.tuser[95:32] = wg_cnt;
+					s_outp.tuser[5] = 1;
+					s_outp.tuser[4:0] = s_inp.tuser;
+				end
+			end
+			
+			BYPASS_1, BYPASS_2, BYPASS_3: begin
+				s_outp.tvalid = s_inp.tvalid;
+				s_outp.tdata = s_inp.tdata;
+				s_outp.tkeep = s_inp.tkeep;
+				s_outp.tlast = s_inp.tlast;
+				s_outp.tuser[4:0] = s_inp.tuser;
+			end
+
+			PAYLOAD_5: begin
+				s_outp.tvalid = s_inp.tvalid;
+				s_outp.tdata[127:48] = s_inp.tdata[79:0];
+				s_outp.tdata[47:0] = wg_enp;
+				s_outp.tkeep = '1;
+				s_outp.tlast = s_inp.tlast;
+				s_outp.tuser[127:96] = wg_rcv;
+				s_outp.tuser[95:32] = wg_cnt;
+				s_outp.tuser[5] = 1;
+				s_outp.tuser[4:0] = s_inp.tuser;
+			end
+			
+			default: begin
+				s_outp.tvalid = 0;
+				s_outp.tdata = '0;
+				s_outp.tkeep = '0;
+				s_outp.tlast = 0;
+				s_outp.tuser = '0;
+			end
+		endcase
+    end
+	assign s_inp.tready = s_outp.tready;
+	assign is_idle = (state == IDLE) & !outp.tvalid;
+	
+	// Packet pipeline
+    axis_pipeline_register #(
+        .DATA_WIDTH(TDATA_WIDTH),
+        .USER_WIDTH(INP_TUSER_WIDTH),
+		.LENGTH(3),
+		.REG_TYPE(2)
+    ) pipeline (
+        .clk(clk),
+        .rst(rst),
+        .s_axis_tvalid(inp.tvalid),
+        .s_axis_tready(inp.tready),
+        .s_axis_tdata(inp.tdata),
+        .s_axis_tkeep(inp.tkeep),
+        .s_axis_tlast(inp.tlast),
+        .s_axis_tuser(inp.tuser),
+        .s_axis_tid('0),
+        .s_axis_tdest('0),
+        .m_axis_tvalid(s_inp.tvalid),
+        .m_axis_tready(s_inp.tready),
+        .m_axis_tdata(s_inp.tdata),
+        .m_axis_tkeep(s_inp.tkeep),
+        .m_axis_tlast(s_inp.tlast),
+        .m_axis_tuser(s_inp.tuser),
+        .m_axis_tid(),
+        .m_axis_tdest()
+    );
+    
+    // Skid buffer
+    axis_register #(
+        .DATA_WIDTH(TDATA_WIDTH),
+        .USER_WIDTH(OUTP_TUSER_WIDTH)
+    ) sbuff (
+        .clk(clk),
+        .rst(rst),
+        .s_axis_tvalid(s_outp.tvalid),
+        .s_axis_tready(s_outp.tready),
+        .s_axis_tdata(s_outp.tdata),
+        .s_axis_tkeep(s_outp.tkeep),
+        .s_axis_tlast(s_outp.tlast),
+        .s_axis_tuser(s_outp.tuser),
+        .s_axis_tid('0),
+        .s_axis_tdest('0),
+        .m_axis_tvalid(outp.tvalid),
+        .m_axis_tready(outp.tready),
+        .m_axis_tdata(outp.tdata),
+        .m_axis_tkeep(outp.tkeep),
+        .m_axis_tlast(outp.tlast),
+        .m_axis_tuser(outp.tuser),
+        .m_axis_tid(),
+        .m_axis_tdest()
+    );
+endmodule
---
+++ 1.hw/ip.dpe/sim/dpe_wg_diassembler/tb.sv
@@ -0,0 +1,223 @@
+//==========================================================================
+// Copyright (C) 2024-2025 Chili.CHIPS*ba
+//--------------------------------------------------------------------------
+//                      PROPRIETARY INFORMATION
+//
+// The information contained in this file is the property of CHILI CHIPS LLC.
+// Except as specifically authorized in writing by CHILI CHIPS LLC, the holder
+// of this file: (1) shall keep all information contained herein confidential;
+// and (2) shall protect the same in whole or in part from disclosure and
+// dissemination to all third parties; and (3) shall use the same for operation
+// and maintenance purposes only.
+//--------------------------------------------------------------------------
+// Description: 
+//   DPE WireGuard Disassembler testbench
+//==========================================================================
+
+`timescale 1ps/1ps
+
+module tb;
+    // Constants
+    localparam CLK_PERIOD = 12_500;
+    localparam DATA_WIDTH = 128;
+    localparam KEEP_WIDTH = DATA_WIDTH/8;
+    localparam INP_USER_WIDTH = 5;
+	localparam OUTP_USER_WIDTH = 128;
+    
+    // Clock and reset signals
+    logic clk = 0;
+    logic rst = 1;
+	logic is_idle;
+    
+    // Interfaces
+    dpe_if #(DATA_WIDTH, INP_USER_WIDTH) inp();
+    dpe_if #(DATA_WIDTH, OUTP_USER_WIDTH) outp();
+    
+    // Test data type and constants
+    typedef logic [127:0] test_data_t [11];
+    typedef test_data_t packet_data_t [4];
+    
+    const packet_data_t packet_data = '{
+        '{128'h00050008F85ABFE5363A83B59434E6A2, 
+		  128'h090A0100090A26631140000017039C00,
+		  128'hF40600000001AE1488006CCAC6A90200,
+		  128'hCF2C3786BC020000000000000002AB7D,
+		  128'hD2302D3D2334C570347A3263B7398A20,
+		  128'h989CF26AF51C16BD8C5466DC08A9DB53,
+		  128'h85E58342D52EAB3CABF4B5FF0AF991C3,
+		  128'h08A437F52A662C69698CAA1B663078ED,
+		  128'h70BBF2FAB15757F16B1983B4E1E38051,
+		  128'h3319107F4160B4F8E66A004C3BBE7331,
+		  128'h0000000000005430B61799C89613B7E3},
+		'{128'h00450008F85ABFE5363A83B59434E6A2, 
+		  128'h090A0100090A26631140000017039C00,
+		  128'hF40600000004AE1488006CCAC6A90200,
+		  128'hCF2C3786BC020000000000000002AB7D,
+		  128'hD2302D3D2334C570347A3263B7398A20,
+		  128'h989CF26AF51C16BD8C5466DC08A9DB53,
+		  128'h85E58342D52EAB3CABF4B5FF0AF991C3,
+		  128'h08A437F52A662C69698CAA1B663078ED,
+		  128'h70BBF2FAB15757F16B1983B4E1E38051,
+		  128'h3319107F4160B4F8E66A004C3BBE7331,
+		  128'h0000000000005430B61799C89613B7E3},
+		'{128'h00450008F85ABFE5363A83B59434E6A2, 
+		  128'h090A0100090A26631140000017039C00,
+		  128'hF40600000001AE1488006CCAC6A90200,
+		  128'hCF2C3786BC020000000000000002AB7D,
+		  128'hD2302D3D2334C570347A3263B7398A20,
+		  128'h989CF26AF51C16BD8C5466DC08A9DB53,
+		  128'h85E58342D52EAB3CABF4B5FF0AF991C3,
+		  128'h08A437F52A662C69698CAA1B663078ED,
+		  128'h70BBF2FAB15757F16B1983B4E1E38051,
+		  128'h3319107F4160B4F8E66A004C3BBE7331,
+		  128'h0000000000005430B61799C89613B7E3},
+		'{128'h00450008F85ABFE5363A83B59434E6A2, 
+		  128'h090A0100090A275D1140000076093C00,
+		  128'hF406000000044E1428006CCAC6A90200,
+		  128'h1D65B6E256190000000000000003AB7D,
+		  128'h0000000000007196D0A09230B29CF5D4,
+		  '0,
+		  '0,
+		  '0,
+		  '0,
+		  '0,
+		  '0}
+    };
+    
+    // Clock generation
+    always #(CLK_PERIOD/2) clk = ~clk;
+    
+    // DUT instantiation
+    dpe_wg_disassembler DUT (
+        .clk(clk),
+        .rst(rst),
+		.is_idle(is_idle),
+        .inp(inp),
+        .outp(outp)
+    );
+    
+    // Main stimulus process
+    initial begin
+        // Initialize input interface
+        inp.tvalid = 0;
+        inp.tlast = 0;
+        inp.tkeep = '0;
+        inp.tdata = '0;
+               
+        // Reset assertion
+        #(CLK_PERIOD * 4);
+        @(posedge clk);
+        #1ps;
+        rst = 0;
+        #(CLK_PERIOD);
+        
+        // Input stimulus
+        begin
+            @(posedge clk);
+            // Send packet 0
+            #1ps;
+            inp.tvalid = 1;
+            for (int i = 0; i < 11; i++) begin
+                inp.tuser = 5'b00001;
+                inp.tdata = packet_data[0][i];
+                inp.tkeep = (i == 10)?16'h03FF:16'hFFFF;
+                inp.tlast = (i == 10);
+                @(posedge clk);
+                while (!inp.tready) @(posedge clk);
+                #1ps;
+            end
+			inp.tvalid = 0;
+            inp.tlast = 0;
+			inp.tkeep = '0;
+			inp.tdata = '0;
+			// Send packet 1
+            inp.tvalid = 1;
+            for (int i = 0; i < 11; i++) begin
+                inp.tuser = 5'b00010;
+                inp.tdata = packet_data[1][i];
+                inp.tkeep = (i == 10)?16'h03FF:16'hFFFF;
+                inp.tlast = (i == 10);
+                @(posedge clk);
+                while (!inp.tready) @(posedge clk);
+                #1ps;
+            end
+			inp.tvalid = 0;
+            inp.tlast = 0;
+			inp.tkeep = '0;
+			inp.tdata = '0;
+			// Send packet 2
+            inp.tvalid = 1;
+            for (int i = 0; i < 11; i++) begin
+                inp.tuser = 5'b00100;
+                inp.tdata = packet_data[2][i];
+                inp.tkeep = (i == 10)?16'h03FF:16'hFFFF;
+                inp.tlast = (i == 10);
+                @(posedge clk);
+                while (!inp.tready) @(posedge clk);
+                #1ps;
+            end
+			inp.tvalid = 0;
+            inp.tlast = 0;
+			inp.tkeep = '0;
+			inp.tdata = '0;
+			// Send packet 3
+            inp.tvalid = 1;
+            for (int i = 0; i < 5; i++) begin
+                inp.tuser = 5'b00100;
+                inp.tdata = packet_data[3][i];
+                inp.tkeep = (i == 4)?16'h03FF:16'hFFFF;
+                inp.tlast = (i == 4);
+                @(posedge clk);
+                while (!inp.tready) @(posedge clk);
+                #1ps;
+            end
+			inp.tvalid = 0;
+            inp.tlast = 0;
+			inp.tkeep = '0;
+			inp.tdata = '0;
+        end
+        
+        #(CLK_PERIOD * 7);
+        $display("Stimulus completed successfully");
+    $finish(2);
+    end
+    
+    // Output ready control process
+    initial begin
+        @(posedge clk);
+        #1ps;
+        outp.tready = 1;
+        #(CLK_PERIOD * 10);
+        outp.tready = 0;
+        #CLK_PERIOD;
+        outp.tready = 1;
+        #(CLK_PERIOD * 16);
+        outp.tready = 0;
+        #(CLK_PERIOD * 2);
+        outp.tready = 1;
+        /*#(CLK_PERIOD * 4);
+        outp.tready = 0;
+        #(CLK_PERIOD * 2);
+        outp.tready = 1;
+        #(CLK_PERIOD * 6);
+        outp.tready = 0;
+        #(CLK_PERIOD * 2);
+        outp.tready = 1;*/
+    end
+    
+    // Monitor process
+    int expected_data_count = 0;
+    
+    always @(posedge clk) begin
+        if (!rst) begin
+            if (outp.tvalid && outp.tready) begin
+                expected_data_count++;
+                
+                if (outp.tlast) begin
+                    $display("Packet received with %0d words", expected_data_count);
+                    expected_data_count = 0;
+                end
+            end
+        end
+    end
+endmodule
