34241be9d0dbec6585574b2d339b55ae4ea8db2a
Enio Kaljic <enio.kaljic@gmail.com>
2025-02-04 09:06:35 +0100

    Updated DPE WireGuard Disassembler (wip)

--- 1.hw/ip.dpe/dpe_wg_disassembler.sv
+++ 1.hw/ip.dpe/dpe_wg_disassembler.sv
@@ -13,29 +13,36 @@
 // Description: 
 //   DPE WireGuard Disassembler
 //
-//                        |---------------------------------------->
-//                        |                                is_idle
-//                     --------
-//   inp               |      |----------|
-//  ------------------>| FSMD |          |
-//      |              |      |----|    \v
-//      |              --------    |--->|\         ----------
+//  ---------------------. .--------------------------------------->
+//   wg_idx              | |                                wg_rcv
+//  -------------------. | | .------------------------------------->
+//   wg_match          | | | |                            fcr_idle
+//                     v v v v
+//                    .-------.
+//   inp              |       |----------.
+//  ----+------------>| FSMD  |          |
+//      |             |       |----.    \v
+//      |             '-------'    '--->|\         .--------.
 //      |                 ^             | |        |  SKID  |
-//      |  ------------   |             | |------->| BUFFER |------>
-//      |  | PIPELINE |   |             | | s_outp | DEPTH=1| outp
-//      |->|  BUFFER  |---------------->|/         ----------
-//         |  DEPTH=3 | s_inp           /
-//         ------------                 MUX
+//      |  .----------.   |             | |------->| BUFFER |------>
+//      |  | PIPELINE |   |             | | muxed  | DEPTH=1| outp
+//      '->|  BUFFER  |---+------------>|/         '--------'
+//         |  DEPTH=3 | piped           /
+//         '----------'                 MUX
 //==========================================================================
 
 module dpe_wg_disassembler (
-    input  logic      clk,
-    input  logic      rst,
+    input  logic        clk,
+    input  logic        rst,
     
-    output logic      is_idle,
+    output logic        fcr_idle,
+	
+	output logic [31:0] wg_rcv,
+	input  logic [6:0]  wg_idx,
+	input  logic        wg_idx_match,
     
-    dpe_if.s_axis     inp,
-    dpe_if.m_axis     outp
+    dpe_if.s_axis       inp,
+    dpe_if.m_axis       outp
 );
     localparam TDATA_WIDTH = 128;
     localparam INP_TUSER_WIDTH = 5;
@@ -44,8 +51,7 @@ module dpe_wg_disassembler (
     localparam PROT_IP = 16'h0008;
     localparam PROT_IPv4 = 4'h4;
     localparam PROT_UDP = 8'h11;
-    localparam PROT_WG = 16'h6cca;
-    localparam PROT_WGD = 8'h04;
+    localparam PROT_WG = 32'h00000004;
     
     typedef enum logic [3:0] {
         IDLE,
@@ -62,168 +68,168 @@ module dpe_wg_disassembler (
     } state_t;
     
     // Internal states
-    state_t state, next_state;
-    logic [31:0] wg_rcv, next_wg_rcv;
-    logic [63:0] wg_cnt, next_wg_cnt;
-    logic [47:0] wg_enp, next_wg_enp;
+    state_t state_reg, state_next;
+    logic [31:0] wg_rcv_reg, wg_rcv_next;
+    logic [63:0] wg_cnt_reg, wg_cnt_next;
+    logic [47:0] wg_enp_reg, wg_enp_next;
     
     // Internal wires
-    dpe_if #(TDATA_WIDTH, INP_TUSER_WIDTH) s_inp();
-    dpe_if #(TDATA_WIDTH, OUTP_TUSER_WIDTH) s_outp();
+    dpe_if #(TDATA_WIDTH, INP_TUSER_WIDTH) piped();
+    dpe_if #(TDATA_WIDTH, OUTP_TUSER_WIDTH) muxed();
     
     // FSM registers
     always_ff @(posedge clk) begin
         if (rst) begin
-            state <= IDLE;
-            wg_rcv <= '0;
-            wg_cnt <= '0;
-            wg_enp <= '0;
+            state_reg <= IDLE;
+            wg_rcv_reg <= '0;
+            wg_cnt_reg <= '0;
+            wg_enp_reg <= '0;
         end else begin
-            state <= next_state;
-            wg_rcv <= next_wg_rcv;
-            wg_cnt <= next_wg_cnt;
-            wg_enp <= next_wg_enp;
+            state_reg <= state_next;
+            wg_rcv_reg <= wg_rcv_next;
+            wg_cnt_reg <= wg_cnt_next;
+            wg_enp_reg <= wg_enp_next;
         end
     end
     
     // FSM transition logic
     always_comb begin
-        next_state = state;
-        next_wg_rcv = wg_rcv;
-        next_wg_cnt = wg_cnt;
-        next_wg_enp = wg_enp;
+        state_next = state_reg;
+        wg_rcv_next = wg_rcv_reg;
+        wg_cnt_next = wg_cnt_reg;
+        wg_enp_next = wg_enp_reg;
         
         case (state)
             IDLE: begin
                 if (inp.tvalid && inp.tready && (inp.tdata[111:96] == PROT_IP) && (inp.tdata[119:116] == PROT_IPv4)) begin
-                    next_state = HEADER_1;
+                    state_next = HEADER_1;
                 end else if (inp.tvalid && inp.tready) begin
-                    next_state = BYPASS_1;
+                    state_next = BYPASS_1;
                 end
             end
             
             HEADER_1: begin
                 if (inp.tvalid && inp.tready && (inp.tdata[63:56] == PROT_UDP)) begin
-                    next_state = HEADER_2;
+                    state_next = HEADER_2;
                 end else if (inp.tvalid && inp.tready) begin
-                    next_state = BYPASS_2;
+                    state_next = BYPASS_2;
                 end
             end
             
             HEADER_2: begin
-                if (inp.tvalid && inp.tready && ((inp.tdata[47:32] == PROT_WG) || (inp.tdata[31:16] == PROT_WG)) && (inp.tdata[87:80] == PROT_WGD)) begin
-                    next_state = PAYLOAD_1;
-                    next_wg_rcv[15:0] = inp.tdata[127:112];
+                if (inp.tvalid && inp.tready && (inp.tdata[111:80] == PROT_WG)) begin
+                    state_next = PAYLOAD_1;
+                    wg_rcv_next[15:0] = inp.tdata[127:112];
                 end else if (inp.tvalid && inp.tready) begin
-                    next_state = BYPASS_3;
+                    state_next = BYPASS_3;
                 end
             end
             
             BYPASS_1: begin
                 if (inp.tvalid && inp.tready) begin
-                    next_state = BYPASS_2;
+                    state_next = BYPASS_2;
                 end
             end
             
             BYPASS_2: begin
                 if (inp.tvalid && inp.tready) begin
-                    next_state = BYPASS_3;
+                    state_next = BYPASS_3;
                 end
             end
             
             BYPASS_3: begin
                 if (inp.tvalid && inp.tready && inp.tlast) begin
-                    next_state = IDLE;
+                    state_next = IDLE;
                 end
             end
             
             PAYLOAD_1: begin
                 if (inp.tvalid && inp.tready) begin
-                    next_state = PAYLOAD_2;
-                    next_wg_rcv[31:16] = inp.tdata[15:0];
-                    next_wg_cnt = inp.tdata[79:16];
+                    state_next = PAYLOAD_2;
+                    wg_rcv_next[31:16] = inp.tdata[15:0];
+                    wg_cnt_next = inp.tdata[79:16];
                 end
             end
             
             PAYLOAD_2: begin
                 if (inp.tvalid && inp.tready) begin
-                    next_state = PAYLOAD_3;
+                    state_next = PAYLOAD_3;
                 end
             end
             
             PAYLOAD_3: begin
-                if (s_inp.tvalid && s_inp.tready) begin
-                    next_state = PAYLOAD_4;
+                if (piped.tvalid && piped.tready) begin
+                    state_next = PAYLOAD_4;
                 end
             end
             
             PAYLOAD_4: begin
-                if (s_inp.tvalid && s_inp.tready && s_inp.tlast) begin
-                    next_state = IDLE;
-                    next_wg_enp = s_inp.tdata[127:80];
-                end else if (s_inp.tvalid && s_inp.tready) begin
-                    next_state = PAYLOAD_5;
-                    next_wg_enp = s_inp.tdata[127:80];
+                if (piped.tvalid && piped.tready && piped.tlast) begin
+                    state_next = IDLE;
+                    wg_enp_next = piped.tdata[127:80];
+                end else if (piped.tvalid && piped.tready) begin
+                    state_next = PAYLOAD_5;
+                    wg_enp_next = piped.tdata[127:80];
                 end
             end
             
             PAYLOAD_5: begin
-                if (s_inp.tvalid && s_inp.tready && s_inp.tlast) begin
-                    next_state = IDLE;
-                    next_wg_enp = s_inp.tdata[127:80];
-                end else if (s_inp.tvalid && s_inp.tready) begin
-                    next_wg_enp = s_inp.tdata[127:80];
+                if (piped.tvalid && piped.tready && piped.tlast) begin
+                    state_next = IDLE;
+                    wg_enp_next = piped.tdata[127:80];
+                end else if (piped.tvalid && piped.tready) begin
+                    wg_enp_next = piped.tdata[127:80];
                 end
             end
             
             default: begin
-                next_state = state;
-                next_wg_rcv = wg_rcv;
-                next_wg_cnt = wg_cnt;
-                next_wg_enp = wg_enp;
+                state_next = state_reg;
+                wg_rcv_next = wg_rcv_reg;
+                wg_cnt_next = wg_cnt_reg;
+                wg_enp_next = wg_enp_reg;
             end
         endcase
     end
     
     // Outputs logic
     always_comb begin
-        s_outp.tvalid = 0;
-        s_outp.tdata = '0;
-        s_outp.tkeep = '0;
-        s_outp.tlast = 0;
-        s_outp.tuser = '0;
+        muxed.tvalid = 0;
+        muxed.tdata = '0;
+        muxed.tkeep = '0;
+        muxed.tlast = 0;
+        muxed.tuser = '0;
         case (state)
             IDLE, HEADER_1, HEADER_2, BYPASS_1, BYPASS_2, BYPASS_3: begin
-                s_outp.tvalid = s_inp.tvalid;
-                s_outp.tdata = s_inp.tdata;
-                s_outp.tkeep = s_inp.tkeep;
-                s_outp.tlast = s_inp.tlast;
-                s_outp.tuser[4:0] = s_inp.tuser;
+                muxed.tvalid = piped.tvalid;
+                muxed.tdata = piped.tdata;
+                muxed.tkeep = piped.tkeep;
+                muxed.tlast = piped.tlast;
+                muxed.tuser[4:0] = piped.tuser;
             end
 
             PAYLOAD_5: begin
-                s_outp.tvalid = s_inp.tvalid;
-                s_outp.tdata[127:48] = s_inp.tdata[79:0];
-                s_outp.tdata[47:0] = wg_enp;
-                s_outp.tkeep = '1;
-                s_outp.tlast = s_inp.tlast;
-                s_outp.tuser[127:96] = wg_rcv;
-                s_outp.tuser[95:32] = wg_cnt;
-                s_outp.tuser[5] = 1;
-                s_outp.tuser[4:0] = s_inp.tuser;
+                muxed.tvalid = piped.tvalid;
+                muxed.tdata[127:48] = piped.tdata[79:0];
+                muxed.tdata[47:0] = wg_enp_reg;
+                muxed.tkeep = '1;
+                muxed.tlast = piped.tlast;
+                muxed.tuser[127:96] = wg_rcv_reg;
+                muxed.tuser[95:32] = wg_cnt_reg;
+                muxed.tuser[5] = 1;
+                muxed.tuser[4:0] = piped.tuser;
             end
             
             default: begin
-                s_outp.tvalid = 0;
-                s_outp.tdata = '0;
-                s_outp.tkeep = '0;
-                s_outp.tlast = 0;
-                s_outp.tuser = '0;
+                muxed.tvalid = 0;
+                muxed.tdata = '0;
+                muxed.tkeep = '0;
+                muxed.tlast = 0;
+                muxed.tuser = '0;
             end
         endcase
     end
-    assign s_inp.tready = s_outp.tready;
-    assign is_idle = (state == IDLE) & !outp.tvalid;
+    assign piped.tready = muxed.tready;
+    assign fcr_idle = (state == IDLE) & !outp.tvalid;
     
     // Packet pipeline
     axis_pipeline_register #(
@@ -242,12 +248,12 @@ module dpe_wg_disassembler (
         .s_axis_tuser(inp.tuser),
         .s_axis_tid('0),
         .s_axis_tdest('0),
-        .m_axis_tvalid(s_inp.tvalid),
-        .m_axis_tready(s_inp.tready),
-        .m_axis_tdata(s_inp.tdata),
-        .m_axis_tkeep(s_inp.tkeep),
-        .m_axis_tlast(s_inp.tlast),
-        .m_axis_tuser(s_inp.tuser),
+        .m_axis_tvalid(piped.tvalid),
+        .m_axis_tready(piped.tready),
+        .m_axis_tdata(piped.tdata),
+        .m_axis_tkeep(piped.tkeep),
+        .m_axis_tlast(piped.tlast),
+        .m_axis_tuser(piped.tuser),
         .m_axis_tid(),
         .m_axis_tdest()
     );
@@ -259,12 +265,12 @@ module dpe_wg_disassembler (
     ) sbuff (
         .clk(clk),
         .rst(rst),
-        .s_axis_tvalid(s_outp.tvalid),
-        .s_axis_tready(s_outp.tready),
-        .s_axis_tdata(s_outp.tdata),
-        .s_axis_tkeep(s_outp.tkeep),
-        .s_axis_tlast(s_outp.tlast),
-        .s_axis_tuser(s_outp.tuser),
+        .s_axis_tvalid(muxed.tvalid),
+        .s_axis_tready(muxed.tready),
+        .s_axis_tdata(muxed.tdata),
+        .s_axis_tkeep(muxed.tkeep),
+        .s_axis_tlast(muxed.tlast),
+        .s_axis_tuser(muxed.tuser),
         .s_axis_tid('0),
         .s_axis_tdest('0),
         .m_axis_tvalid(outp.tvalid),
