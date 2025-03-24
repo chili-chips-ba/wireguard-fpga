5a1d6c4b1816ec41f9d38f9312cd7d5c69baa719
eniokaljic <enio.kaljic@gmail.com>
2025-03-24 11:19:39 +0100

    Added GPIO CSR test application

---
+++ 2.sw/app/csr_gpio_test/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.c
+ * @brief Test program for GPIO CSR
+ */
+
+#include <stdint.h>
+#include "csr.h"
+
+void delay(uint32_t count) {
+   volatile uint32_t i;
+   for (i = 0; i < count; i++);
+}
+
+int main(void)
+{
+   volatile csr_t *csr = (volatile csr_t*)0x20000000;
+   
+   while (1) {
+      csr->gpio.f.led2 = 1;
+      csr->gpio.f.led1 = 0;
+      delay(1000000);
+      csr->gpio.f.led2 = 0;
+      csr->gpio.f.led1 = 1;
+      delay(1000000);
+   }
+   
+   return 0;
+}
