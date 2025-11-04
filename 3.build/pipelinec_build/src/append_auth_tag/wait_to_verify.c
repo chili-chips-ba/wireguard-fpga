41c46928e980f7cd981a8831785b30f2b1966791
nsokolovic7 <nikola.sokolovic@chili-chips.xyz>
2025-11-04 19:13:35 +0100

    Working on wait_to_verify

--- 3.build/pipelinec_build/src/append_auth_tag/wait_to_verify.c
+++ 3.build/pipelinec_build/src/append_auth_tag/wait_to_verify.c
@@ -1,6 +1,6 @@
 // Append the auth tag after the ciphertext
 #include "arrays.h"
-#include "fifo.h"
+#include "global_fifo.h"
 
 // Input stream of plaintext
 stream(axis128_t) wait_to_verify_axis_in; // input
@@ -19,76 +19,49 @@ typedef enum wait_to_verify_state_t{
   OUTPUT_PLAINTEXT //once verify bit arrives output plaintext and verify bit
 }wait_to_verify_state_t;
 
-// First word through as a stream() fifo
-// FIFO_FWFT(name, type_t, DEPTH)
-FIFO_FWFT(verify_fifo, axis128_t, 128);
+// GLOBAL_STREAM_FIFO(data_t, name, DEPTH)
+GLOBAL_STREAM_FIFO(axis128_t, verify_fifo, 128)
 
 #pragma MAIN wait_to_verify
 void wait_to_verify()
 {
-  
-  static wait_to_verify_state_t state = WAIT_FOR_VERIFY_BIT;
-  // Reg to hold the recieved verification result (tags_match)
+  static wait_to_verify_state_t state = WAIT_TO_VERIFY_BIT;
+  // Reg to hold teh recieved verification result (tags_match)
   static uint1_t tags_match_reg = 0;
-  // Reg to track if the verify bit has been successfully recieved
-  static uint1_t verify_bit_recieved = 0;
 
-  // Control signals
-  // The FIFO read 
-
-  // Declaring the FIFO
-  verify_fifo_t fifo_out = verify_fifo(
-  
-
-  // Conncet inputs to the FIFO
-  // the ready for the output stream
-  // (aka file read enable)
-  wait_to_verify_axis_out_ready,
-
+  // Write side of FIFO
   // the data+valid for input stream
   // (aka fifo write data, write enable)
-  wait_to_verify_axis_in.data, 
-  wait_to_verify_axis_in.valid
-);
-  // Connect outputs from FIFO
-  // the data+valid for output stream
-  // (aka fifo read data, not empty signal)
-  wait_to_verify_axis_out.data = fifo_out.data_out;
-  wait_to_verify_axis_out.valid = fifo_out.data_out_valid;
-
+  verify_fifo_in = wait_to_verify_axis_in;
   // the ready signal for the input stream
-  // (aka the fifo not full signal)
-  wait_to_verify_axis_in_ready = fifo_out.data_in_ready;
+  // (aka fifo not full signal)
+  wait_to_verify_axis_in_ready = verify_fifo_in_ready;
 
+  // Read side of FIFO
+  // the data+valid for output stream
+  // (aka fifo read data, not eampty signal)
+  wait_to_verify_axis_out = verify_fifo_out;
+  // the ready for the output stream
+  // (aka fifo read enable)
+  verify_fifo_out_ready = wait_to_verify_axis_out_ready;
   
-
-  // Default not ready for incoming data
-  wait_to_verify_axis_in_ready = 0;
-  wait_to_verify_verify_bit_ready = 0;
-  
-  // Default not outputting data
-  stream(axis128_t) axis128_null = {0};
-  wait_to_verify_axis_out = axis128_null;
+  // Default not ready for the single verify bit input
+  wait_to_verify_bit_ready = 0;
+  // Default FIFO Read Enable is set by FSM
+  verify_fifo_out_ready = 0;
+  // Default output valid/tlast for stream is 0
+  wait_to_verify_axis_out.valid = 0;
+  wait_to_verify_axis_out.data.tlast = 0;
+  // Default is_verified output
   wait_to_verify_is_verified_out = 0;
-  
-  // Plaintext data always comes from the FIFO
-  wait_to_verify_axis_out.date = fifo_out.data_out;
 
   if(state == WAIT_FOR_VERIFY_BIT)
   {
-    // Plaintext buffering
-    // Input is ready only if the FIFO is not full and
-    // we haven't recieved teh bit yet
-    if(!verify_bit_recieved)
-    {
-      // Ready signal for the plaintext input comes from FIFO
-      wait_to_verify_axis_in_ready = fifo_out.data_in_ready;
-    }
-    else
-    {
-      // Stop recieving plaintext input after the verify arrives
-      wait_to_verify_axis_in_ready = 0;
-    }
+    // FIFO Read Enable: Disconnected, data stays buffered
+    verify_fifo_out_ready = 0;
+
+    // Output stream: invalid
+    wait_to_verify_axis_out.valid = 0;
 
     // Wait for verify bit
     // Be ready to accept the single bit
@@ -98,14 +71,10 @@ void wait_to_verify()
     {
       // Capture the result and signal reception
       tags_match_reg = wait_to_verify_bit.data;
-      verify_bit_recieved = 1;
 
       // Move to output state
       state = OUTPUT_PLAINTEXT;
     }
-
-    // Output valid is always 0 in this state
-    wait_to_verify_axis_out.valid = 0;
   }
 
   else //if(state == OUTPUT_PLAINTEXT)
@@ -113,24 +82,28 @@ void wait_to_verify()
     // Plaintext output
     // Valid only if FIFO is not empty
     wait_to_verify_axis_out.valid = fifo_out.data_out_valid;
+    //tlast comes directly from the FIFO data
+    wait_to_verify_axis_out.data.tlast = verify_fifo_out.data.tlast;
 
     // Verified bit output
-    // Valid only when plaintext output is valid
-    wait_to_verify_is_veriied_out = tags_match_reg;
+    // Synchronize with stream valid
+    if (wait_to_verify_axis_out.valid)
+    {
+      wait_to_verify_is_verified_out = tags_match_reg;
+    }
 
-    if(wait_to_verify_axis_out.valid & output_ready_combined)
+    // Check for successful consumption of the last packet
+    if(wait_to_verify_axis_out.valid && wait_to_verify_axis_out_ready)
     {
       // If output is consumed, check for tlast
       if (wait_to_verify_axis_out.data.tlast)
       {
         // End of buffered plaintext, reset the FSM
-	state = WAIT_FOR_VERIFY_BIT;
-	verify_bit_recieved = 0;
+        state = WAIT_FOR_VERIFY_BIT;
       }  
     }
 
     // Input ready's are disconnected in this state
-    wait_to_verify_axis_in_ready = 0;
     wait_to_verify_verify_bit_ready = 0;
   }
 }
