3a3812d0493d22cfdc338fc8e0f5c025777212d7
kerimbavcic <kerim.bavcic@chili-chips.xyz>
2025-11-19 22:07:32 +0100

    adapting the blake2s path in hkdf component

--- 99.warmup/11.hkdf/hkdf.h
+++ 99.warmup/11.hkdf/hkdf.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#include "../blake2s_rv32/blake2s.h"
-#include "string_bare.h"        
+#include "../BLAKE2s/rv32/blake2s.h"
+#include "string_bare.h"
 
 /* Noise uses 32-byte hashes (BLAKE2s-256) */
 #define NOISE_HASH_LEN 32
@@ -15,11 +15,10 @@
  * Any output may be NULL or length = 0 to skip it.
  */
 void kdf(
-    uint8_t *first_dst,  size_t first_len,
+    uint8_t *first_dst, size_t first_len,
     uint8_t *second_dst, size_t second_len,
-    uint8_t *third_dst,  size_t third_len,
+    uint8_t *third_dst, size_t third_len,
     const uint8_t *data, size_t data_len,
-    const uint8_t chaining_key[NOISE_HASH_LEN]
-);
+    const uint8_t chaining_key[NOISE_HASH_LEN]);
 
 #endif // HKDF_H
