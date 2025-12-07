ecd540ed24f8247cfb26a004106d66261ad44a05
nsokolovic7 <nikola.sokolovic@chili-chips.xyz>
2025-12-07 12:15:00 +0100

    Removing unncessary files

--- 3.build/pipelinec_build/generated-files-sim-pipe-dec/built_in/VAR_REF_ASSIGN_uint32_t_chacha20_state_state_VAR_23da/clock.xdc
+++
@@ -1,1 +0,0 @@
-create_clock -add -name clk -period 1.0 -waveform {0 0.5} [get_ports {clk}]
--- 3.build/pipelinec_build/generated-files-sim-pipe-dec/type_bytes_t.h/type_bytes_t.h/type_bytes_t.h
+++
@@ -1,4 +0,0 @@
-
-typedef uint8_t type_bytes_t; // DUMMY
-#define type_size_t uint32_t // DUMMY
-#define TYPE_TO_BYTES(so,dumb) // DUMMY
